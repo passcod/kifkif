0fb23afb50fbd6320c085df7984cd756908682a7
Félix Saparelli <felix@passcod.name>
2014-04-16 01:31:03 +1200

    Use global CORS

--- web.rb
+++ web.rb
@@ -1,10 +1,13 @@
 class Kifkif::Web < Sinatra::Base
+  configure do
+    enable :cross_origin
+  end
+
   get '/' do
     "Hello!"
   end
 
   post '/diff' do
-    cross_origin allow_methods: [:get]
     request.body.rewind
     puts request.body.read
   end
